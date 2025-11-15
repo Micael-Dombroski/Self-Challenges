7b56af6317f2870db37d8956ef5351cb342540b0
Dust C <137210879+Micael-Dombroski@users.noreply.github.com>
2025-11-15 20:58:59 -0300

    Implement Huffman coding: compress .txt files and decompress .bin files

--- huffman-coding/huffman.c
+++ huffman-coding/huffman.c
@@ -20,7 +20,7 @@ typedef struct {
 //frequency table
 void fill_frequency_table(unsigned char text[], unsigned int tab[]) {
     int i = 0;
-    while(text[i] != '\n') {
+    while(text[i] != '\0') {
         tab[text[i]]++;
         i++;
     }
@@ -47,7 +47,7 @@ void ordered_insert(List *list, Node *node) {
             list -> start = node;
         } else {
             Node *sup = list -> start;
-            while(sup -> next && sup -> next -> frequency < node -> frequency) {
+            while(sup -> next && sup -> next -> frequency <= node -> frequency) {
                 sup = sup -> next;
             }
             node -> next = sup -> next;
@@ -60,10 +60,10 @@ void ordered_insert(List *list, Node *node) {
 }
 
 void fill_list(unsigned int tab[], List *list) {
-    Node *new;
-    for(int i = 0; i < SIZE; i++) {
+    for(int i = 0; i< SIZE; i++) {
         if(tab[i] > 0) {
-            if((new = calloc(1, sizeof(Node)))) {
+            Node *new = malloc(sizeof(Node));
+            if(new) {
                 new -> charater = i;
                 new -> frequency = tab[i];
                 new -> next = NULL;
@@ -71,8 +71,9 @@ void fill_list(unsigned int tab[], List *list) {
                 new -> right = NULL;
                 ordered_insert(list, new);
             } else {
-                printf("\n\tFailed to allocate memory on fill_list\n");
-            }  
+                printf("\tFailed to allocate memory in fill_list\n");
+                break;
+            }
         }
     }
 }
@@ -80,9 +81,9 @@ void fill_list(unsigned int tab[], List *list) {
 void print_list(List *list) {
     if(list -> start) {
         Node *sup = list -> start;
-        printf("\n\tOrder List (Size: %d)\n", list -> size);
+        printf("\n\tOrdered list: Size: %d\n", list -> size);
         while(sup) {
-            printf("\tCharacter :%c\tFrequency: %u\n", sup -> charater, sup -> frequency);
+            printf("\tCharacter: %c Frequency %u\n", sup -> charater, sup -> frequency);
             sup = sup -> next;
         }
     }
@@ -105,7 +106,8 @@ Node* build_tree(List *list) {
     while(list -> size > 1) {
         first = remove_start_node(list);
         second = remove_start_node(list);
-        if((new = malloc(sizeof(Node)))) {
+        new = malloc(sizeof(Node));
+        if(new) {
             new -> charater = '+';
             new -> frequency = first -> frequency + second -> frequency;
             new -> left = first;
@@ -122,21 +124,21 @@ Node* build_tree(List *list) {
 
 void print_tree(Node *root, int size) {
     if(!root -> left && !root -> right) {
-        printf("\tLeaft: %c\tHeight: %d\n", root -> charater, size);
+        printf("\tLeaf: %c\tHeight: %d\n", root -> charater, size);
     } else {
         print_tree(root -> left, size + 1);
-        print_tree(root -> right, size + 1);
+        print_tree(root -> right, size +1);
     }
 }
 
 int tree_height(Node *root) {
     if(root) {
-        int height_left = tree_height(root -> left);
-        int height_right = tree_height(root -> right);
-        if(height_left > height_right) {
-            return height_left + 1;
+        int hLeft = tree_height(root -> left);
+        int hRight = tree_height(root -> right);
+        if(hLeft > hRight) {
+            return hLeft + 1;
         } else {
-            return height_right + 1;
+            return hRight + 1;
         }
     } else {
         return -1;
@@ -241,11 +243,27 @@ char* decoding(char *code, Node *root) {
 }
 
 //compacting
-void compact(unsigned char str[], char name[]) {
+void compact(unsigned char str[], char name[], unsigned int freq_table[]) {
     FILE *file = fopen(name, "wb");
     int i = 0, j = 7;
     unsigned char byte = 0, mask;
+    unsigned int buffer_size = 0;
+    int compact_bytes = 0;
     if(file) {
+        unsigned char *buffer = malloc(1);
+        for(i = 0; i < SIZE; i++) {
+            if(freq_table[i] > 0) {
+                unsigned char c = (unsigned char) i;
+                fwrite(&c, sizeof(unsigned char), 1, file);
+                fwrite(&freq_table[i], sizeof(unsigned int), 1, file);
+            }
+        }
+        unsigned char end = 0xFF;
+        unsigned int zero = 0;
+
+        fwrite(&end, sizeof(unsigned char), 1, file);
+        fwrite(&zero, sizeof(unsigned int), 1, file);
+        i = 0;
         while(str[i] != '\0') {
             mask = 1;
             if(str[i] == '1') {
@@ -255,14 +273,39 @@ void compact(unsigned char str[], char name[]) {
             i++;
             j--;
             if(j < 0) {
-                fwrite(&byte, sizeof(unsigned char), 1, file);
+                unsigned char *tmp = realloc(buffer, buffer_size + 1);
+                if(!tmp) {
+                    printf("\n\tFailed to realloc memory on compactin\n");
+                    free(buffer);
+                    fclose(file);
+                    return;
+                }
+                buffer = tmp;
+                buffer[buffer_size++] = byte;
+                compact_bytes++;
                 byte = 0;
                 j = 7;
             }
         }
         if(j != 7) {
-            fwrite(&byte, sizeof(unsigned char), 1, file);
+            unsigned char *tmp = realloc(buffer, buffer_size + 1);
+            if(!tmp) {
+                printf("\n\tFailed to realloc memory on compactin\n");
+                free(buffer);
+                fclose(file);
+                return;
+            }
+            buffer = tmp;
+            buffer[buffer_size++] = byte;
+            compact_bytes++;
         }
+        unsigned char valid_bits = 8 - (j + 1); 
+        fwrite(&valid_bits, sizeof(unsigned char), 1, file);
+        fwrite(&compact_bytes, sizeof(unsigned int), 1, file);
+        for(i = 0; i < buffer_size; i++) {
+            fwrite(&buffer[i], sizeof(unsigned char), 1, file);
+        }
+        free(buffer);
         fclose(file);
     } else {
         printf("\n\tFailed to open/creat the file on compact\n");
@@ -288,30 +331,77 @@ unsigned int is_bit_one(unsigned char byte, int i) {
     return byte & mask;
 }
 
+void read_freq_table(FILE *file, unsigned int freq_table[]) {
+    unsigned char c;
+    unsigned int freq;
+    while(1) {
+        if(fread(&c, sizeof(unsigned char), 1, file) != 1) {
+            printf("\n\tError on read_freq_table\n");
+            return;
+        }
+        if(fread(&freq, sizeof(unsigned int), 1, file) != 1) {
+            printf("\n\tError on read_freq_table\n");
+            return;
+        }
+        if(c == 0xFF && freq == 0) {
+            break;
+        }
+        freq_table[c] = freq;
+    }
+}
+
 //descompacting
-void descompact(Node *root) {
+unsigned char* descompact(char *file_name, unsigned int freq_table[]) {
+    unsigned char *text = calloc(1, sizeof(unsigned char));
+    char str[2];
     FILE *file;
-    if((file = fopen("file.wg", "rb"))) {
+    if((file = fopen(file_name, "rb"))) {
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        rewind(file);
+        printf("\n\tGetting frequency table...\n");
+        read_freq_table(file, freq_table);
+        unsigned char last_bits;
+        fread(&last_bits, sizeof(unsigned char), 1, file);
+        unsigned int compact_bytes;
+        fread(&compact_bytes, sizeof(unsigned int), 1, file);
+        List list;
+        printf("\n\tCreating the list...\n");
+        create_list(&list);
+        fill_list(freq_table, &list);
+        printf("\n\tBuilding binary-tree...\n");
+        Node *root = build_tree(&list);
         Node *sup = root;
         int i = 0;
         unsigned char byte;
+        printf("\n\tDescompacting the text...\n");
         while(fread(&byte, sizeof(unsigned char), 1, file)) {
-            for(i = 7; i >= 0; i--) {
+            int max_bits = 8;
+            if (compact_bytes == 0) {
+                max_bits = last_bits;
+            }
+            for (i = 7; i >= 8 - max_bits; i--) {
                 if(is_bit_one(byte, i)) {
                     sup = sup -> right;
                 } else {
                     sup = sup -> left;
                 }
                 if(!sup -> left && !sup -> right) {
-                    printf("%c", sup -> charater);
+                    str[0] = sup -> charater;
+                    str[1] = '\0';
+                    strcat(text, str);
+                    text = realloc(text, (strlen(text) + 2));
                     sup = root;
                 }
             }
+            compact_bytes--;
         }
         fclose(file);
     } else {
         printf("\n\tFailed to open the file on descompact\n");
+        return NULL;
     }
+    return text;
 }
 
 void get_file_name(char name[]) {
@@ -333,8 +423,9 @@ long file_size(char name[]) {
 }
 
 char* read_file(char name[], char *extension) {
-    char *file_name = calloc((strlen(name) + strlen(extension)), sizeof(char));
+    char *file_name = malloc(sizeof(char) * ((strlen(name) + strlen(extension)) + 1));
     if(file_name) {
+        file_name[0] = '\0';
         strcat(file_name, name);
         strcat(file_name, extension);
         printf("%s\n", file_name);
@@ -344,7 +435,7 @@ char* read_file(char name[], char *extension) {
         }
         FILE *file;
         printf("File size: %ld\n", size);
-        if((file = fopen(file_name, strcmp(extension, ".bin") == 0 || strcmp(extension, ".wd") ? "rb" : "r\0"))) {
+        if((file = fopen(file_name, (strcmp(extension, ".bin") == 0 || strcmp(extension, ".wd") == 0) ? "rb" : "r"))) {
             char *text, byte;
             if((text = calloc(size, sizeof(char)))) {
                 while(fread(&byte, sizeof(char), 1, file)) {
@@ -357,8 +448,8 @@ char* read_file(char name[], char *extension) {
                 printf("\n\tFailed to allocate memory on read_file\n");
                 return NULL;
             }
-            return text;
             fclose(file);
+            return text;
         } else {
             printf("\n\tFailed to open file on read_file\n");
             return NULL;
@@ -378,22 +469,34 @@ int get_positive_integer() {
     return num;
 }
 
+void free_tree(Node *root) {
+    if (!root) return;
+    free_tree(root->left);
+    free_tree(root->right);
+    free(root);
+}
+
 void compact_file_menu() {
     int opt, collumns;
     char file_name[50], *content = NULL, **dictionary = NULL;
     unsigned int frequency_table[SIZE] = {0};
     List list;
-    list.size = 0;
-    list.start = NULL;
-    Node *tree;
+    create_list(&list);
+    Node *tree = NULL;
     do {
         printf("\n\t====Compact File Menu====\n");
-        printf("\t[1] Compact\n\t[2] Show Frequency Table\n\t[3] Show List\n\t[4] Show Binary-Tree\n\t[5] File Content\n\t[6] Dictionary\n\t[0] Return\n");
+        printf("\t[1] Compact\n\t[2] Show Frequency Table\n\t[3] Show List\n\t[4] File Content\n\t[5] Dictionary\n\t[0] Return\n");
         opt = get_positive_integer();
         switch (opt)
         {
         case 1:
+            if(content) {
+                free(content);
+                content = NULL;
+            }
             printf("Enter the file name: \n");
+            memset(frequency_table, 0, sizeof(frequency_table));
+            create_list(&list);
             get_file_name(file_name);
             content = read_file(file_name, ".txt");
             if(content) {
@@ -416,14 +519,20 @@ void compact_file_menu() {
             }
             char *file_comp = calloc((strlen(file_name) + 5), sizeof(char));
             strcat(file_comp, file_name);
-            strcat(file_comp, ".wd");
-            compact(content, file_comp);
+            strcat(file_comp, ".bin");
+            char *code_content = coding(dictionary, content);
+            compact(code_content, file_comp, frequency_table);
+            free(file_comp);
+            free(code_content);
             break;
         case 2:
-            print_frequency_table(frequency_table);
+            if(content) {
+                print_frequency_table(frequency_table);
+            } else {
+                printf("\n\tThere is no content to create the frequency table\n");
+            }
             break;
         case 3:
-            fill_list(frequency_table, &list);
             if(list.start) {
                 print_list(&list);
             } else {
@@ -431,9 +540,6 @@ void compact_file_menu() {
             }
             break;
         case 4:
-            print_tree(tree, tree_height(tree));
-            break;
-        case 5:
             printf("File content: \n");
             if(content) {
                 printf("%s\n", content);
@@ -441,8 +547,12 @@ void compact_file_menu() {
                 printf("\n\tThere is no content on the file\n");
             }
             break;
-        case 6:
-            print_dictionary(dictionary); 
+        case 5:
+            if(tree) {
+                print_dictionary(dictionary); 
+            } else {
+                printf("\n\tYou can't have a dictionary with an empty binary-tree\n");
+            }
             break;
         case 0:
             printf("\nReturning...\n");
@@ -452,10 +562,59 @@ void compact_file_menu() {
             break;
         }
     } while (opt != 0);
+    Node *sup = list.start;
+    while(sup) {
+        Node *node = sup;
+        sup = node -> next;
+        free(node);
+    }
+    if(tree) {
+        for(int i = 0; i < SIZE; i++) {
+            free(dictionary[i]);
+        }
+        free(dictionary);
+        free_tree(tree);
+    }
+    free(content);
 }
 
 void descompact_file_menu() {
-
+    int opt;
+    char file_name[50], *content = NULL;
+    unsigned int frequency_table[SIZE] = {0};
+    do {
+        printf("\n\t=====Descompact File Menu=====\n");
+        printf("\t[1] Descompact\n\t[2] Show Content\n\t[0] Return\n");
+        opt = get_positive_integer();
+        switch (opt)
+        {
+        case 1:
+            if(content) {
+                free(content);
+                content = NULL;
+            }
+            printf("Enter the file name: \n");
+            get_file_name(file_name);
+            strcat(file_name, ".bin");
+            memset(frequency_table, 0, sizeof(frequency_table));
+            content = descompact(file_name, frequency_table);
+            if(!content) {
+                printf("\n\tThere is no content to create the frequency table\n");
+            }
+            break;
+        case 2:
+            printf("\nDescompacted content: \n");
+            printf("%s\n", content);
+            break;
+        case 0:
+            printf("\n\tReturning...\n");
+            break;
+        default:
+            printf("Invalid option\n");
+            break;
+        }
+    } while(opt != 0);
+    free(content);
 }
 
 int main(void) {
@@ -470,7 +629,7 @@ int main(void) {
             compact_file_menu();
             break;
         case 2:
-            
+            descompact_file_menu();
             break;
         case 0:
             printf("\n\tExiting...\n");
